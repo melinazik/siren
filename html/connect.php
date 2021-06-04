48f87846fe349db9a9584d601837727944800397
Chris <christpc@csd.auth.gr>
2021-06-04 16:20:41 +0300

    register works for xammp localhost

--- html/connect.php
+++ html/connect.php
@@ -1,19 +1,29 @@
 <?php 
-    $email = $_POST['email'];
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $password_repeat = $_POST['password_repeat'];
 
-    //Database connection
-    $conn = new mysqli('localhost', 'root', '', 'webtest');
-    if($conn->connect_error){
-        die('Connection failed : '.$conn->connect_error);
-    } else{
-        $stmt = $conn ->prepare("insert into registration(email,username,password,password_repeat) values(?,?,?,?)");
-        $stmt->bind_param("ssss", $email,$username, $password,$password_repeat);
-        $stmt->execute();
-        echo "registration successful";
-        $stmt->close();
-        $conn->close();
-    }
+    session_start();
+    $username = "";
+    $email = "";
+    $pwd = "";
+    $password_repeat="";
+
+    $errors = array();
+
+    //connecting to db
+
+    $db = mysqli_connect('localhost', 'root', '','webtest') or die("could not connect to db");
+
+    //registering user
+
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $pwd = mysqli_real_escape_string($db, $_POST['pwd']);
+    $password_repeat = mysqli_real_escape_string($db, $_POST['password_repeat']);
+
+    $pwd_encrypted = md5($pwd); //password encrypted
+    $query = "INSERT INTO registration (email, username, pwd) VALUES ('$email','$username', '$pwd_encrypted')";
+    mysqli_query($db, $query);
+    $_SESSION['username'] = $username;
+    $_SESSION['success'] = "You are now registered";
+    
+    echo"SUCCESS!!!"
 ?>
