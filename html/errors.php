af505e57e28681f33fdb25713a1b182927f5893c
Chris <christpc@csd.auth.gr>
2021-06-05 22:00:24 +0300

    testing some transitions from html to php documents

---
+++ html/errors.php
@@ -0,0 +1,6 @@
+<?php
+
+    if(count($errors)>0) :
+        
+
+?> 
--- html/login.php
+++ html/login.php
@@ -1,3 +1,29 @@
+<?php
+ 
+// Starting the session, to use and
+// store data in session variable
+session_start();
+  
+// If the session variable is empty, this
+// means the user is yet to login
+// User will be sent to 'login.php' page
+// to allow the user to login
+if (!isset($_SESSION['username'])) {
+    $_SESSION['msg'] = "You have to log in first";
+    header('location: login.php');
+}
+  
+// Logout button will destroy the session, and
+// will unset the session variables
+// User will be headed to 'login.php'
+// after loggin out
+if (isset($_GET['logout'])) {
+    session_destroy();
+    unset($_SESSION['username']);
+    header("location: login.php");
+}
+?>
+
 <!DOCTYPE html>
 <html lang ="en">
   <head>
@@ -42,7 +68,10 @@
         </li>
         <li><a href="help.html">How to help</a></li>
         <li><a href="contact.html">Contact us</a></li>
-        <li><a href="login.html">Login/Register</a></li>
+        <li><a href="login.php">Login/Register</a></li>
+        <?php  if (isset($_SESSION['username'])) : ?>
+          <li><div><?php echo $_SESSION['username'];?></div></li>
+          <?php endif ?>
 
       </ul>
       <a href="javascript:void(0);" class="icon" onclick="myFunction()">
