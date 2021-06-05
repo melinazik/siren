b4ad14bb7991e58ae70bc21d338767c05b6f4e3d
Chris <christpc@csd.auth.gr>
2021-06-05 21:01:50 +0300

    log in now works too, need to add some more checks

--- html/login.php
+++ html/login.php
@@ -7,20 +7,35 @@
     
     //connecting to db
     $db = mysqli_connect('localhost', 'root', '','webtest') or die("could not connect to db");
+    
+    if(isset($_POST['submit'])){
+        $username = mysqli_real_escape_string($db, $_POST['username']);
+        $pwd = mysqli_real_escape_string($db, $_POST['pwd']);
 
-    //getting user's input
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-    $pwd = mysqli_real_escape_string($db, $_POST['pwd']);
-    
-    if(empty($username)){array_push($errors, "Username required");}
-    if(empty($pwd)){array_push($errors, "password required");}
-    
-    //form validation and insertion
-    $user_check_query = "SELECT * FROM registration WHERE username = '$username' or email='$email' LIMIT 1";
-    $results = mysqli_query($db, $user_check_query);
-    $user = mysqli_fetch_assoc($results);
-    if(!$user){
-        if($user['username']===$username){array_push($errors, "Username doesn't exist");}
+        if(empty($username)){
+            array_push($errors, "Username is required");
+        
         }
-    
+
+        if(empty($pwd)){
+            array_push($errors, "Password is required");
+        
+        }
+
+        if(count($errors)==0){
+            $pwd = md5($pwd);
+            $query = "SELECT * FROM registration WHERE username='$username' AND pwd='$pwd'";
+            $results = mysqli_query($db, $query);
+
+            if(mysqli_num_rows($results)){
+                $_SESSION['username'] = $username;
+                $_SESSION['success'] = "You are now logged in!";
+                header('location: home.html');
+            } else{
+                array_push($errors, "Wrong password or username, please try again.");
+                header('location: help.html');
+            }
+
+        }
+    }
 ?>
