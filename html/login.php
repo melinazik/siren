bbab4dc50bcf6a7e5b2ede2a622cd844054a520a
Chris <christpc@csd.auth.gr>
2021-06-06 15:33:49 +0300

    removed excess comments

--- html/login.php
+++ html/login.php
@@ -1,27 +1,4 @@
 <?php
- 
-// Starting the session, to use and
-// store data in session variable
-//session_start();
-  
-// If the session variable is empty, this
-// means the user is yet to login
-// User will be sent to 'login.php' page
-// to allow the user to login
-//if (!isset($_SESSION['username'])) {
-   // $_SESSION['msg'] = "You have to log in first";
-   // header('location: login.php');
-//}
-  
-// Logout button will destroy the session, and
-// will unset the session variables
-// User will be headed to 'login.php'
-// after loggin out
-/*if (isset($_GET['logout'])) {
-    session_destroy();
-    unset($_SESSION['username']);
-    header("location: login.php");
-}*/
 include('server.php');
 ?>
 
