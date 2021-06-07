aa378a58ca1f2cef24e96f7fa53447ba669bca40
Chris <christpc@csd.auth.gr>
2021-06-07 11:03:06 +0300

    show username if logged in, show login/registered if not

--- html/login.php
+++ html/login.php
@@ -34,8 +34,9 @@
   				</li>
   				<li><a href="help.php">How to help</a></li>
   				<li><a href="contact.php">Contact us</a></li>
-  				<li><a href="login.php">Login/Register</a></li>
-          <?php if (isset($_SESSION['username'])) : ?>
+  				<?php if (!isset($_SESSION['username'])) : ?>
+          <li><a href="login.php">Login/Register</a></li>
+          <?php else: ?>
         <li><a href="profile.php"><?php echo $_SESSION['username'];?></a></li>
         <?php endif ?>
   			</ul>
