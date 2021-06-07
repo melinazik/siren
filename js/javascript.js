8d48169a6b00e8ab02cc6fbcf09027df8323b736
melinaz@live.com <melinaz@live.com>
2021-06-07 18:03:26 +0300

    Revert "Revert "Add files via upload""
    This reverts commit 1277dfed98f08e19e098a04d5846fc1d7c438fdc.

--- js/javascript.js
+++ js/javascript.js
@@ -1,55 +1,56 @@
 // function to "dive" from home page to articles
-window.smoothScroll = function (target) {
-  var scrollContainer = target;
-  do { //find scroll container
-    scrollContainer = scrollContainer.parentNode;
-    if (!scrollContainer) return;
-    scrollContainer.scrollTop += 1;
-  } while (scrollContainer.scrollTop == 0);
+window.smoothScroll = function(target) {
+    var scrollContainer = target;
+    do { //find scroll container
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
 
-  var targetY = 0;
-  do { //find the top of target relatively to the container
-    if (target == scrollContainer) break;
-    targetY += target.offsetTop;
-  } while (target = target.offsetParent);
+    var targetY = 0;
+    do { //find the top of target relatively to the container
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
 
-  scroll = function (c, a, b, i) {
-    i++; if (i > 30) return;
-    c.scrollTop = a + (b - a) / 30 * i;
-    setTimeout(function () { scroll(c, a, b, i); }, 20);
-  }
-  // start scrolling
-  scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+    scroll = function(c, a, b, i) {
+        i++; if (i > 30) return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 20);
+    }
+    // start scrolling
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
 
 function myFunction() {
-  var x = document.getElementById("navbarID");
-  if (x.className === "right-navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "right-navbar";
+    var x = document.getElementById("navbarID");
+    if (x.className === "right-navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "right-navbar";
+    }
   }
-}
 
-// loading icon
-$(window).on("load", function () {
-  $(".loader").fadeOut("slow");
-});
+  // loading icon
+  $(window).on("load", function(){
+    $(".loader").fadeOut("slow");
+  });
 
-// favorites
-function addFavorites(element) {
-  // element is the carousel-image-container element that called 
-  // addFavorites with onclick
+  const imgDiv = document.querySelector('.profile-img');
+  const img = document.querySelector('#photo-prof');
+  const file = document.querySelector('#file');
+  const uploadBtn = document.querySelector('#upload-btn');
 
-  var src = element.childNodes[1].childNodes[1].childNodes[1].getAttribute("src");
-  if (src == "../imgs/heart-empty.png") {
-    element.childNodes[1].childNodes[1].childNodes[1].src = '../imgs/heart-full.png';
-    element.childNodes[1].childNodes[1].childNodes[3].innerHTML = "remove from favorites";
-  } else {
-    element.childNodes[1].childNodes[1].childNodes[1].src = '../imgs/heart-empty.png';
-    element.childNodes[1].childNodes[1].childNodes[3].innerHTML = "add to favorites";
+  file.addEventListener('change', function(){
+    const chosenFile = this.files[0];
 
-<<<<<<< HEAD
+    if(chosenFile){
+      const reader = new FileReader();
+
+      reader.addEventListener('load',function(){
+        img.setAttribute('src', reader.result);
+      });
+
       reader.readAsDataURL(chosenFile);
     }
   });
@@ -66,7 +67,3 @@ function addFavorites(element) {
     element.childNodes[1].childNodes[1].childNodes[3].innerHTML = "add to favorites";
 
   }
-=======
-  }
-}
->>>>>>> parent of d0cc48d... Add files via upload
