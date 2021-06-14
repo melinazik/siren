629a93759eea85cad51bbcc28c8107195d7a83b9
melinaz@live.com <melinaz@live.com>
2021-06-14 22:30:28 +0300

    update causes.php file - same as effects

--- html/causes.php
+++ html/causes.php
@@ -152,22 +152,37 @@
 				<!-- END CAROUSEL -->
 
 				<?php $query = "SELECT * FROM article";
-				$results = mysqli_query($db, $query);
-				$articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
-				$size = count($articles);
-				$articlesJSON = array();
-				$userId = $_SESSION['userId'];
+			$results = mysqli_query($db, $query);
+			$articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+			$size = count($articles);
+			$articlesJSON = array();
+			$userId = $_SESSION['userId'];
 
-				echo "<script> init(); </script>";
+			$favorite = 0;
 
-				for ($i = 0; $i < $size; $i++) {
-					array_push($articlesJSON, json_encode($articles[$i]));
-					echo "<script>var add = loadEffectsArticles(
-						$articlesJSON[$i].articleTitle, $articlesJSON[$i].articleURL,
-						$articlesJSON[$i].articleImg, $articlesJSON[$i].numberOfLikes,
-						$articlesJSON[$i].favorite, $userId, $articlesJSON[$i].id); </script>";
+			echo "<script> init(); </script>";
+
+			for ($i = 0; $i < $size; $i++) {
+				array_push($articlesJSON, json_encode($articles[$i]));
 				
+				$articleId = $articles[$i]['id'];
+				$query1 = "SELECT * FROM userlikesarticle WHERE userId = $userId and articleId = $articleId";
+				$results1 = mysqli_query($db, $query1);
+
+
+				if(mysqli_num_rows($results1)){
+					$favorite = 1;
 				}
+				else{
+					$favorite = 0;
+				}
+
+				echo "<script>var add = loadEffectsArticles(
+					$articlesJSON[$i].articleTitle, $articlesJSON[$i].articleURL,
+					$articlesJSON[$i].articleImg, $articlesJSON[$i].numberOfLikes,
+					$favorite, $userId, $articlesJSON[$i].id); </script>";
+			}
+
 			?>
 		</div>
 
