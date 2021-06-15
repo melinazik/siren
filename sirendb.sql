700832b77c411a32777cbcae3b74cf5e195d76c1
melinaz@live.com <melinaz@live.com>
2021-06-15 22:28:49 +0300

    update db

--- sirendb.sql
+++ sirendb.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2021 at 07:36 PM
--- Server version: 10.4.19-MariaDB
+-- Generation Time: Jun 15, 2021 at 09:28 PM
+-- Server version: 10.4.19-MariaDB-log
 -- PHP Version: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -33,7 +33,6 @@ CREATE TABLE `article` (
   `articleImg` varchar(8000) DEFAULT NULL COMMENT 'image path',
   `numberOfLikes` int(11) DEFAULT NULL,
   `articleTitle` varchar(100) DEFAULT NULL,
-  `favorite` tinyint(1) NOT NULL DEFAULT 0,
   `category` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,19 +40,19 @@ CREATE TABLE `article` (
 -- Dumping data for table `article`
 --
 
-INSERT INTO `article` (`id`, `articleURL`, `articleImg`, `numberOfLikes`, `articleTitle`, `favorite`, `category`) VALUES
-(11, 'https://www.nationalgeographic.com/environment/article/plastic-pollution', '../imgs/plastic_waste_natgeo.PNG', 2, 'The World\'s plastic pollution explained', 0, 'effects'),
-(12, 'https://ocean.si.edu/ocean-life/invertebrates/ocean-acidification', '../imgs/coral-reef.jpg', 1, 'Ocean Acidification', 0, 'effects'),
-(13, 'https://www.nationalgeographic.com/environment/article/critical-issues-marine-pollution', '../imgs/marine_polution.PNG', 0, 'Marine Pollution Explained', 0, 'effects'),
-(14, 'https://www.nationalgeographic.com/environment/article/great-barrier-reef-restoration-transplanting-corals', '../imgs/coral_reef.PNG', 1, 'Can new science save dying coral reefs?', 0, 'effects'),
-(15, 'https://www.nationalgeographic.com/environment/article/critical-issues-overfishing', '../imgs/overfishing_natgeo.PNG', 1, 'Plenty of fish in the sea? Not always.', 0, 'effects'),
-(16, 'https://www.noaa.gov/education/resource-collections/ocean-coasts/oil-spills', '../imgs/oil_spills.PNG', 1, 'Oil Spills', 0, 'effects'),
-(17, 'https://www.wayfairertravel.com/inspiration/worlds-most-endangered-marine-species/', '../imgs/sea_otter.PNG', 1, '10 of the world\'s most endangered marine species', 0, 'causes'),
-(18, 'https://www.worldwildlife.org/stories/how-climate-change-relates-to-oceans', '../imgs/ocean_sunset.PNG', 1, 'How climate change relates to the oceans', 0, 'causes'),
-(19, 'https://www.nrdc.org/stories/global-climate-change-what-you-need-know', '../imgs/bear_iceberg.PNG', 0, 'Global climate change; What you need to know', 0, 'causes'),
-(20, 'https://www.conserve-energy-future.com/causes-effects-solutions-depleting-marine-life.php', '../imgs/fish_ocean.PNG', 1, 'What is marine life?', 0, 'causes'),
-(21, 'https://www.nationalgeographic.com/magazine/article/plastic-planet-health-pollution-waste-microplastics', '../imgs/plastic_bottle.PNG', 1, 'We know plastic is hurting marine life. What about us?', 0, 'causes'),
-(22, 'https://www.wwf.org.au/news/blogs/what-do-sea-turtles-eat-unfortunately-plastic-bags#gs.z3r46c', '../imgs/turtle_plastic.PNG', 1, 'What do sea turtles eat? Unfortunately, plastic bags.', 0, 'causes');
+INSERT INTO `article` (`id`, `articleURL`, `articleImg`, `numberOfLikes`, `articleTitle`, `category`) VALUES
+(11, 'https://www.nationalgeographic.com/environment/article/plastic-pollution', '../imgs/plastic_waste_natgeo.PNG', 2, 'The World\'s plastic pollution explained', 'effects'),
+(12, 'https://ocean.si.edu/ocean-life/invertebrates/ocean-acidification', '../imgs/coral-reef.jpg', 1, 'Ocean Acidification', 'effects'),
+(13, 'https://www.nationalgeographic.com/environment/article/critical-issues-marine-pollution', '../imgs/marine_polution.PNG', 0, 'Marine Pollution Explained', 'effects'),
+(14, 'https://www.nationalgeographic.com/environment/article/great-barrier-reef-restoration-transplanting-corals', '../imgs/coral_reef.PNG', 1, 'Can new science save dying coral reefs?', 'effects'),
+(15, 'https://www.nationalgeographic.com/environment/article/critical-issues-overfishing', '../imgs/overfishing_natgeo.PNG', 1, 'Plenty of fish in the sea? Not always.', 'effects'),
+(16, 'https://www.noaa.gov/education/resource-collections/ocean-coasts/oil-spills', '../imgs/oil_spills.PNG', 1, 'Oil Spills', 'effects'),
+(17, 'https://www.wayfairertravel.com/inspiration/worlds-most-endangered-marine-species/', '../imgs/sea_otter.PNG', 1, '10 of the world\'s most endangered marine species', 'causes'),
+(18, 'https://www.worldwildlife.org/stories/how-climate-change-relates-to-oceans', '../imgs/ocean_sunset.PNG', 1, 'How climate change relates to the oceans', 'causes'),
+(19, 'https://www.nrdc.org/stories/global-climate-change-what-you-need-know', '../imgs/bear_iceberg.PNG', 0, 'Global climate change; What you need to know', 'causes'),
+(20, 'https://www.conserve-energy-future.com/causes-effects-solutions-depleting-marine-life.php', '../imgs/fish_ocean.PNG', 1, 'What is marine life?', 'causes'),
+(21, 'https://www.nationalgeographic.com/magazine/article/plastic-planet-health-pollution-waste-microplastics', '../imgs/plastic_bottle.PNG', 1, 'We know plastic is hurting marine life. What about us?', 'causes'),
+(22, 'https://www.wwf.org.au/news/blogs/what-do-sea-turtles-eat-unfortunately-plastic-bags#gs.z3r46c', '../imgs/turtle_plastic.PNG', 1, 'What do sea turtles eat? Unfortunately, plastic bags.', 'causes');
 
 -- --------------------------------------------------------
 
@@ -65,19 +64,18 @@ CREATE TABLE `messages` (
   `id` int(11) NOT NULL,
   `contactName` varchar(100) NOT NULL,
   `contactEmail` varchar(100) NOT NULL,
-  `contactText` varchar(8000) NOT NULL,
-  `isRead` tinyint(1) NOT NULL DEFAULT 0
+  `contactText` varchar(8000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `messages`
 --
 
-INSERT INTO `messages` (`id`, `contactName`, `contactEmail`, `contactText`, `isRead`) VALUES
-(10, 'Kostas', 'kostas@example.com', 'Hi, very nice website! ', 0),
-(11, 'user', 'empty@example.com', 'PWD CHANGE REQUEST', 0),
-(12, 'Michael', 'michael@example.com', 'I have some suggestions to make. If you\'d like to hear them, please email me back on this address', 0),
-(13, 'Lorem Ipsum', 'lorem@ipsum.com', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 0);
+INSERT INTO `messages` (`id`, `contactName`, `contactEmail`, `contactText`) VALUES
+(10, 'Kostas', 'kostas@example.com', 'Hi, very nice website! '),
+(11, 'user', 'empty@example.com', 'PWD CHANGE REQUEST'),
+(12, 'Michael', 'michael@example.com', 'I have some suggestions to make. If you\'d like to hear them, please email me back on this address'),
+(13, 'Lorem Ipsum', 'lorem@ipsum.com', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
 -- --------------------------------------------------------
 
